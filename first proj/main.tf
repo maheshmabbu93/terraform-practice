388ab381aeff21446770832c8838f5a29fe83b1c
maheshmabbu93 <maheshmabbu93@gmail.com>
2023-09-16 12:43:55 +0530

    ec2 instance created

--- first proj/main.tf
+++ first proj/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+    region = "us-east-1" #desired region 
+  
+}
+resource "aws_instance" "mahesh_tf" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
 
+    tags = {
+        Name = "tf-mahesh_tf"
+    } 
+}
